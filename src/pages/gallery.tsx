5fb15753f11a44860249465b235105607cd0b096
Corey Smaller <coreysmaller@Coreys-MacBook-Pro.local>
2021-06-26 13:45:12 -0400

    add layout to image slider

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Carousel from 'react-gallery-carousel';
 import 'react-gallery-carousel/dist/index.css';
-// Components
-// import Layout from '../components/Layout'
-// import Seo from '../components/seo'
-// import Item from '../components/GalleryCard'
-
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import styled from "styled-components";
 
 const images = [];
 for (let i = 1; i < 21; i++){
@@ -14,19 +12,34 @@ for (let i = 1; i < 21; i++){
    
 const thumbWidth="10%";
 const thumbHeight="20%";
-export default function Gallery() {
+const title="Gallery";
+
+const GalleryContainer = styled.div`
+    position:relative;
+    object-fit: none;
+    width:100%;
+
+`;
+
+export default function Gallery(props) {
     return (
-        <>
+        <Layout location={props.location} title={title}>
+        <SEO
+          description="tiling in Utah"
+          lang="en"
+          title="Contact"
+          keywords={[`tiling in utah`, 'cabinets in utah', 'cabinets and tiling']}
+        />
         <section className="text-center py-4">
             <h1> pretty cool gallery huh??</h1>
             <p>you can say stuff here like " our work is frikkin amazing. No Really , its
                 awesome. Everyone loves our shit. i mean, just look at it!"
             </p>
         </section>
-        <div className='carousel-container'>
+        <GalleryContainer>
             <Carousel hasThumbnailsAtMax={false} images={images} thumbnailWidth={thumbWidth} thumbnailHeight={thumbHeight} style={{ height: 500 }} />
-        </div>
-        </>
+        </GalleryContainer>
+        </Layout>
     );
 };
 
